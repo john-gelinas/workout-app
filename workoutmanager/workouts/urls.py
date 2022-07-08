d5a6d96cb0d7dfc602eee5cb8a5807ff0014256b
jhelum13 <jngelinas@yahoo.com>
2022-07-08 12:14:51 -0700

    added routes to django

---
+++ workoutmanager/workouts/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .api import ProfileViewSet, WorkoutViewSet, ExerciseViewSet, ExerciseTypeViewSet, ExerciseCategoryViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/profiles', ProfileViewSet, 'profiles')
+router.register('api/workouts', WorkoutViewSet, 'workouts')
+router.register('api/exercises', ExerciseViewSet, 'exercises')
+router.register('api/exercisetypes', ExerciseTypeViewSet, 'exercisetypes')
+router.register('api/exercisecategories', ExerciseCategoryViewSet, 'exercisecategories')
