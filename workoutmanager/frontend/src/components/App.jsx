da3b03c15b4d6d00024619e9bda4d0bdde7a118b
jhelum13 <jngelinas@yahoo.com>
2022-07-11 17:49:10 -0700

    app structure with footer and workouts

--- workoutmanager/frontend/src/components/App.jsx
+++ workoutmanager/frontend/src/components/App.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Workouts from "./workouts/Workouts";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const App = () => {
-  return (
-    <Header />
-  )
-}
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#1a237e',
+      },
+      secondary: {
+        main: '#81c784',
+      },
+    },
+    typography: {
+      footer: {
+        fontFamily: ['monospace', 'Arial'].join(","),
+        fontSize: '20px'
+      },
+    },
+  });
 
-export default App
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Workouts />
+        <Footer/>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+export default App;
