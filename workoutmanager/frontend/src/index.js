5bedb49e9d1bed8c15f2e892d184aaec4491f435
jhelum13 <jngelinas@yahoo.com>
2022-07-11 17:48:33 -0700

    add redux store

--- workoutmanager/frontend/src/index.js
+++ workoutmanager/frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import App from "./components/App";
 import ReactDom from "react-dom/client";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
