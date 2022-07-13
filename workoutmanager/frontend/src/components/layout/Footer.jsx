f8878443e8d9eebe1e61306d4a1ec88242559e18
jhelum13 <jngelinas@yahoo.com>
2022-07-13 12:54:53 -0700

    footer styling

--- workoutmanager/frontend/src/components/layout/Footer.jsx
+++ workoutmanager/frontend/src/components/layout/Footer.jsx
@@ -1,16 +1,25 @@
-import { AppBar, Typography, Stack } from "@mui/material";
+import { AppBar, Typography, Stack, Button } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
   return (
-    <AppBar position="static" color="primary" sx={{ top: "auto", bottom: 0, p: 2, mt: 2 }}>
-      <Stack direction="row" justifyContent="space-between">
-        <Typography sx={{ color: "white" }} variant="footer" component="span">
+    <AppBar
+      position="fixed"
+      color="primary"
+      sx={{ top: "auto", bottom: 0, p: 1, pl: 2, pr: 2, mt: 2 }}
+    >
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography
+          sx={{ color: "white", display: "inline", verticalAlign: "bottom" }}
+          variant="footer"
+        >
           &copy; John Gelinas 2022
         </Typography>
-        <Typography sx={{ color: "white" }} variant="footer" component="span">
-          Dark Mode
-        </Typography>
+        <Button variant="contained" color="secondary">
+          <Typography sx={{ color: "white" }} variant="footer" component="span">
+            Dark Mode
+          </Typography>
+        </Button>
       </Stack>
     </AppBar>
   );
