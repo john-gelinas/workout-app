08d82efb9d89e86c4bee452a4a58a68377ad5429
jhelum13 <jngelinas@yahoo.com>
2022-07-15 16:22:37 -0700

    formatting

--- workoutmanager/frontend/src/app/themeSlice.jsx
+++ workoutmanager/frontend/src/app/themeSlice.jsx
@@ -1,15 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {mode: "dark"};
+const initialState = { mode: "dark" };
 
 const themeSlice = createSlice({
   name: "theme",
   initialState,
   reducers: {
     switchTheme(state, action) {
-        const theme = action.payload
-        state.mode = theme;
-    }
+      const theme = action.payload;
+      state.mode = theme;
+    },
   },
 });
 
