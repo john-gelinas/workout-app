fa27daaf8fa62b91dbb5b270ae8d05438bccfd83
jhelum13 <jngelinas@yahoo.com>
2022-07-15 17:03:14 -0700

    added routes

--- workoutmanager/frontend/src/components/App.jsx
+++ workoutmanager/frontend/src/components/App.jsx
@@ -6,7 +6,7 @@ import Workout from "./workouts/Workout";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useSelector } from "react-redux";
-import { Paper } from "@mui/material";
+import { Paper, Box } from "@mui/material";
 
 const App = () => {
   const currentMode = useSelector((state) => state.theme.mode);
@@ -32,10 +32,23 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Paper elevation={1}>
+        <Paper elevation={1} sx={{ minHeight: "100vh", borderRadius: 0, display: "flex", flexDirection: "column" }}>
           <Header />
-          <Workouts />
-          <Workout/>
+          <Box sx={{ mb: 3 }}>
+          <Routes>
+            <Route path="/" element={<Workouts />}></Route>
+            <Route path="/workout">
+              <Route path=":id" element={<Workout />}></Route>
+            </Route>
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p style={{ textAlign: "center" }}>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes></Box>
           <Footer />
         </Paper>
       </BrowserRouter>
