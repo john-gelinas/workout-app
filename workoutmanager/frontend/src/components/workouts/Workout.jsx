fd1b453589d55ec2d1f25f543796016d9df96426
jhelum13 <jngelinas@yahoo.com>
2022-07-18 18:42:47 -0700

    exercise list button

--- workoutmanager/frontend/src/components/workouts/Workout.jsx
+++ workoutmanager/frontend/src/components/workouts/Workout.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Stack } from "@mui/material";
+import ExerciseList from "./ExerciseList";
 import { useParams } from "react-router-dom";
 
 const Workout = () => {
+  const [selectExerciseOpen, setSelectExerciseOpen] = useState(false);
   let { workoutId } = useParams();
+  // fetch exercises
+
+  // get list of exercises in workout
+
+  // create exercise group
+  // card
+  // setform
+
+  // create set for given exercise
+
+  const onAddExercise = (exercise) => {
+    // close exercise list after a quick delay
+    setTimeout(() => setSelectExerciseOpen(false), 200);
+  };
+  const onClickAway = () => {
+    setSelectExerciseOpen(false);
+  };
+
   return (
-    <Stack
-      mt={2}
-      spacing={2}
-      alignItems="center"
-    >
-      <Button id="selectExercise" label="Select Exercise" variant="outlined">
+    <Stack mt={2} spacing={2} alignItems="center">
+      <Button
+        id="selectExercise"
+        label="Select Exercise"
+        variant="outlined"
+        onClick={() => setSelectExerciseOpen(true)}
+      >
         Add Exercise
       </Button>
-      <SetForm exercise={"exercise"}/>
+      {/* workout exercise components */}
+      <ExerciseList
+        selectExerciseOpen={selectExerciseOpen}
+        onClickAway={onClickAway}
+        onAddExercise={onAddExercise}
+      />
     </Stack>
   );
 };
