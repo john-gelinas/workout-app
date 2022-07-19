8a39b4a53e60649c7c6a2ee8ba984278216e0343
jhelum13 <jngelinas@yahoo.com>
2022-07-18 18:43:09 -0700

    new components for exercise groups and sets

---
+++ workoutmanager/frontend/src/components/workouts/WorkoutExercise.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkoutExercise = () => {
+  return (
+    <div>WorkoutExercise</div>
+  )
+}
+
+export default WorkoutExercise
---
+++ workoutmanager/frontend/src/components/workouts/WorkoutExerciseSet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SetForm from "./SetForm";
+
+
+const WorkoutExerciseSet = () => {
+  return (
+    <div>
+      <SetForm exercise={"exercise"} />
+      <div>WorkoutExerciseSet</div>
+    </div>
+  );
+};
+
+export default WorkoutExerciseSet;
