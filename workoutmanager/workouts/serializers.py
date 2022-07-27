c978ca1fccaebc9f3ff09fa806089549d812db10
John Gelinas <jngelinas@yahoo.com>
2022-07-26 23:09:34 -0400

    add exercise type+category to exercise api call

--- workoutmanager/workouts/serializers.py
+++ workoutmanager/workouts/serializers.py
@@ -1,28 +1,53 @@
 from rest_framework import serializers
 from workouts.models import Profile, Workout, ExerciseType, ExerciseCategory, Exercise
 
-#Serializers
+# Serializers
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
 
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
         fields = '__all__'
 
+
 class ExerciseTypeSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category', allow_null=True)
+
     class Meta:
         model = ExerciseType
         fields = '__all__'
+
 
 class ExerciseCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ExerciseCategory
         fields = '__all__'
 
+
 class ExerciseSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(
+        source='exercisetype.category.name', allow_null=True)
+    type = serializers.CharField(
+        source='exercisetype.name', allow_null=True)
+    type_reps = serializers.BooleanField(
+        source='exercisetype.reps', allow_null=True)
+    type_weights = serializers.BooleanField(
+        source='exercisetype.weights', allow_null=True)
+    type_duration = serializers.BooleanField(
+        source='exercisetype.duration', allow_null=True)
+    type_distance = serializers.BooleanField(
+        source='exercisetype.distance', allow_null=True)
+    type_elevation = serializers.BooleanField(
+        source='exercisetype.elevation', allow_null=True)
+    type_image = serializers.BooleanField(
+        source='exercisetype.image', allow_null=True)
+
     class Meta:
         model = Exercise
         fields = '__all__'
