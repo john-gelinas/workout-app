7f05b1e8fc5dc8f232be768dad0d7a4f7c466367
John Gelinas <jngelinas@yahoo.com>
2022-07-30 18:02:29 -0700

    added github icon and footer spacing

--- workoutmanager/frontend/src/components/layout/Footer.jsx
+++ workoutmanager/frontend/src/components/layout/Footer.jsx
@@ -1,19 +1,20 @@
-import { AppBar, Typography, Stack, IconButton } from "@mui/material";
+import { AppBar, Typography, Stack, IconButton, Icon } from "@mui/material";
 import React from "react";
 import { useTheme } from "@mui/material/styles";
 import { useSelector, useDispatch } from "react-redux";
 import { switchTheme } from "../../app/themeSlice";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
   const theme = useTheme();
-  const currentMode = useSelector((state) => state.theme.mode)
+  const currentMode = useSelector((state) => state.theme.mode);
   const dispatch = useDispatch();
   const toggleDarkMode = (currentMode) => {
-    const newMode = currentMode === "dark" ? "light" : "dark"
-    dispatch(switchTheme(newMode))
-  }
+    const newMode = currentMode === "dark" ? "light" : "dark";
+    dispatch(switchTheme(newMode));
+  };
   return (
     <AppBar
       position="static"
@@ -22,17 +23,24 @@ const Footer = () => {
     >
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Typography
-          sx={{ color: "white", display: "inline", verticalAlign: "bottom" }}
+          sx={{ color: "white", display: "inline", verticalAlign: "bottom", mr: "auto" }}
           variant="footer"
         >
           &copy; John Gelinas 2022
         </Typography>
-        <IconButton variant="contained" onClick={(e) => toggleDarkMode(currentMode, e)}>
-          <Typography variant="footer" component="span">
-            {currentMode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-          </Typography>
-        </IconButton>
-
+        <Typography sx={{ color: "white", display: "inline", verticalAlign: "bottom" }} variant="footer" component="span">
+          <IconButton variant="contained">
+            <GitHubIcon />
+          </IconButton>
+        </Typography>
+        <Typography variant="footer" sx={{ color: "white", display: "inline", verticalAlign: "bottom" }} component="span">
+          <IconButton
+            variant="contained"
+            onClick={(e) => toggleDarkMode(currentMode, e)}
+          >
+            {currentMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </Typography>
       </Stack>
     </AppBar>
   );
