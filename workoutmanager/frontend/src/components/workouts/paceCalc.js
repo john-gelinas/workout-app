ea1a2ce1ae16ebe80473e767f638a2d09f2308b0
John Gelinas <jngelinas@yahoo.com>
2022-08-22 20:38:57 -0700

    pace calculation function

--- workoutmanager/frontend/src/components/workouts/paceCalc.js
+++ workoutmanager/frontend/src/components/workouts/paceCalc.js
@@ -1,1 +1,20 @@
 import moment from 'moment';
+
+const paceCalc = (distance, duration) => {
+    if (distance && duration) {
+        const durationSeconds = moment.duration(duration).asSeconds()
+        const paceTotalSeconds = Math.round(durationSeconds / distance);
+        const paceDuration = moment.duration(paceTotalSeconds, 'seconds');
+        const paceHours = ("0" + paceDuration.hours()).slice(-2);
+        const paceMinutes = ("0" + paceDuration.minutes()).slice(-2);
+        const paceSeconds = ("0" + paceDuration.seconds()).slice(-2);
+        const paceString = `${paceHours}:${paceMinutes}:${paceSeconds}`
+        return paceString
+
+    } else {
+        return ""
+    }
+
+}
+
+export default paceCalc
