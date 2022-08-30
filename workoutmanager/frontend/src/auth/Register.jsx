5ee1cc4b23408655aa2e786f03ba3206fc05e1db
John Gelinas <jngelinas@yahoo.com>
2022-08-29 18:12:50 -0700

    register page mui components

--- workoutmanager/frontend/src/auth/Register.jsx
+++ workoutmanager/frontend/src/auth/Register.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useRegisterMutation } from "../api/apiSlice";
-import { userLoginSuccess } from "./authSlice";
-import { useDispatch } from "react-redux/es/exports";
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation, useLogoutMutation, useRegisterMutation } from "../api/apiSlice";
+import { userLoginSuccess, userLoginFail, userLogout } from "./authSlice";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import {
+  TextField,
+  InputAdornment,
+  Typography,
+  Box,
+  Button,
+  CardContent,
+  Paper,
+  Container,
+} from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import KeyIcon from "@mui/icons-material/Key";
+import EmailIcon from '@mui/icons-material/Email';
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -14,6 +27,14 @@ const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const filled = [username, email, password, password2].every(Boolean);
+  const [apiLogout, apiLogoutResult] = useLogoutMutation();
+  const [useLogin, useLoginResult] = useLoginMutation();
+  const authState = useSelector((state) => state.auth);
+
+  const logoutUser = () => {
+    apiLogout();
+    dispatch(userLogout());
+  };
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -24,9 +45,12 @@ const Register = () => {
       if (!filled) {
         throw new Error("Please Fill All Fields");
       }
+      if (authState.isAuthenticated) {
+        logoutUser();
+      }
       const regData = await apiRegister({ email, username, password }).unwrap();
       dispatch(userLoginSuccess(regData));
-      // redirect user to "from" page
+      // redirect user to home page
       navigate("/", { replace: true });
     } catch (error) {
       dispatch(userLoginFail());
@@ -40,62 +64,159 @@ const Register = () => {
   };
 
   return (
-    <div className="col-md-6 m-auto">
-      <div className="card card-body mt-5">
-        <h2 className="text-center">Register</h2>
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Re-Type Password</label>
-            <input
-              type="password"
-              className="form-control"
-              name="password2"
-              value={password2}
-              onChange={(e) => setPassword2(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <button className="btn btn-primary mt-1" type="submit ">
+    <Box sx={{ p: 2 }}>
+      <Paper elevation={3} sx={{ display: "block" }}>
+        <CardContent>
+          <form
+            onSubmit={onSubmit}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignContent: "center",
+              justifyContent: "center",
+              width: "auto",
+            }}
+          >
+            <Typography variant="h2" align="center" gutterBottom>
               Register
-            </button>
-          </div>
-          <p>
-            Already have an account? <Link to="/leadmanager/login">Login</Link>
-          </p>
-        </form>
-      </div>
-    </div>
+            </Typography>
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                required
+                id="username"
+                label="Username"
+                variant="standard"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+              />
+            </Container>
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                required
+                id="email"
+                label="Email"
+                variant="standard"
+                value={username}
+                onChange={(e) => setEmail(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <EmailIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+              />
+            </Container>
+
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                required
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="standard"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Container>
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                required
+                id="password2"
+                label="Re-Type Password"
+                type="password"
+                autoComplete="current-password"
+                variant="standard"
+                value={password}
+                onChange={(e) => setPassword2(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Container>
+
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                mt: 2,
+              }}
+            >
+              <Button type="submit" variant="outlined">
+                Register
+              </Button>
+            </Container>
+
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                mt: 2,
+              }}
+            >
+              <Typography>Already have an account?</Typography>
+            </Container>
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                sx={{ color: "primary.main" }}
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </Button>
+            </Container>
+          </form>
+        </CardContent>
+      </Paper>
+    </Box>
   );
 };
 
 export default Register;
+
+
