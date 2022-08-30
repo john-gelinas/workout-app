002cf756f5668d454ead8692e33cc4e12779fa5d
John Gelinas <jngelinas@yahoo.com>
2022-08-29 18:17:29 -0700

    login page spacing

--- workoutmanager/frontend/src/auth/Login.jsx
+++ workoutmanager/frontend/src/auth/Login.jsx
@@ -115,6 +115,7 @@ const Login = () => {
                     </InputAdornment>
                   ),
                 }}
+                margin="normal"
               />
             </Container>
 
