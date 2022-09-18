eeeaa6c22b054d95f7660b764e5486bbad2eac5d
John Gelinas <jngelinas@yahoo.com>
2022-09-18 13:12:47 -0700

    home page skeleton

--- workoutmanager/frontend/src/components/layout/Home.jsx
+++ workoutmanager/frontend/src/components/layout/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Card>
+      <CardContent>
+        <h1>Home</h1>
+        <p>
+          Record workouts, exercises, and track your PRs. Click the workouts tab
+          to get started.
+        </p>
+        <Link to="/workouts">
+          <Typography>Take Me to Workouts</Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default Home
+export default Home;
