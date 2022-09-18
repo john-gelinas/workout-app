098cfb2a159276b05b7a024f4267dfca2997c643
John Gelinas <jngelinas@yahoo.com>
2022-09-18 11:20:10 -0700

    about page built

--- workoutmanager/frontend/src/components/layout/About.jsx
+++ workoutmanager/frontend/src/components/layout/About.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent } from "@mui/material";
 
 const About = () => {
   return (
-    <div>
-      about
-    </div>
-  )
-}
+    <Card>
+      <CardContent>
+        <h1>About</h1>
+        <p>
+          This website is a personal project built by John Gelinas. It provides
+          a place to track workouts and individual exercises. Calculated one rep
+          maxes, total weight lifted, running pace, and other statistics can be
+          calculated with the data.
+        </p>
+        <h2>Built with:</h2>
+        <ul>
+          <li>Django</li>
+          <li>Django Rest Framework</li>
+          <li>Knox Token Authentication</li>
+          <li>React.js</li>
+          <li>Redux Toolkit & RTK Query</li>
+          <li>Material UI</li>
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default About
+export default About;
